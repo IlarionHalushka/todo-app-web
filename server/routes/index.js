a939bad12c59c8bec2150555983fe5b73f9e4ae2
Ilarion <hilarion.galushka@gmail.com>
2018-10-19 21:30:27 +0300

    updated run scripts for running web app

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-var router = express.Router();
-
-router.get('/', function(req, res) {
-  res.send('<h1> test application 1 </h1>');
-})
-
-module.exports = router;
